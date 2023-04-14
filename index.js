53eae74605c98d17a62a0f9600f7cd124cddc8fd
T3rex <31337aditya@gmail.com>
2023-04-14 14:16:30 +0530

    Code refactor and added logic for authentication failure

--- index.js
+++ index.js
@@ -9,24 +9,40 @@ const axios = require('axios');
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = 3005;
+const AUTHENTICATION_LINK = 'http://localhost:3001/authservice/api/v1/isAuthenticated'
+const AUTH_SERVICE_URL = 'http://localhost:3001'
+const BOOKING_SERVICE_URL = 'http://localhost:3006'
+
+
+
+
 
 const limiter = rateLimit({
-	windowMs: 2 * 60 * 1000, // 15 minutes
-	max: 15, // Limit each IP to 100 requests per `window` (here, per 15 minutes)	
+	windowMs: 2 * 60 * 1000,
+	max: 15, 	
 })
 
 app.use(morgan('combined'));
 app.use(limiter)
 app.use('/bookingservice',async(req,res,next)=>{
+    try {
+        const response = await axios.get(AUTHENTICATION_LINK,{headers: { "x-access-token":  req.headers['x-access-token']}});         
+        if(response.data.success){
+            next();
+        }
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Unauthorized"
+        });
+    }
     
-    const response = await axios.get("http://localhost:3000/authservice/api/v1/isAuthenticated",{headers: { "x-access-token":  req.headers['x-access-token']}});
-    console.log(response.data);    
-    next();
 });
 
-app.use('/authservice', createProxyMiddleware({ target: 'http://localhost:2999',changeOrigin: true }));
-app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3006',changeOrigin: true }));
+app.use('/authservice', createProxyMiddleware({ target: AUTH_SERVICE_URL,changeOrigin: true }));
+app.use('/bookingservice', createProxyMiddleware({ target: BOOKING_SERVICE_URL,changeOrigin: true }));
 
 
 app.get('/home', (req, res) =>{
